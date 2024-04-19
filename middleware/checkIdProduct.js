fe6bbfd9beb76d12513eb5fb35162dac7384e430
Julio Alexandre <julioalexandre499@gmail.com>
2024-04-19 00:07:18 -0300

    adaptando middleware de erro na busca por Id

--- middleware/checkIdProduct.js
+++ middleware/checkIdProduct.js
@@ -2,12 +2,9 @@ const Product = require('../models/productModels');
 
 const checkIdProduct = async(req, res, next) => {
     const { id } = req.params;
-    const product = await Product
-        .findById(id)
-        .catch(err => {
-            return res.status(500).json({ message: err.message });
-        });
-    if (!product) {
+    const products = await Product.find({});
+    const product = products.find(product => product._id == id);
+    if (!product){
         return res.status(404).json({ message: 'Product not found' });
     }
     next();
